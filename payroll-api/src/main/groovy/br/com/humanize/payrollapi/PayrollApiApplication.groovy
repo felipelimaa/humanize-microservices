80b1e8d769f05a8c306306eba949bcd21fa5a1f0
felipe <fearlimasi@gmail.com>
2022-03-02 22:52:58 -0300

    feat: configurated eureka client in payroll-api

--- payroll-api/src/main/groovy/br/com/humanize/payrollapi/PayrollApiApplication.groovy
+++ payroll-api/src/main/groovy/br/com/humanize/payrollapi/PayrollApiApplication.groovy
@@ -2,10 +2,12 @@ package br.com.humanize.payrollapi
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 class PayrollApiApplication {
 
 	static void main(String[] args) {
--- payroll-api/src/main/groovy/br/com/humanize/payrollapi/feignClients/UserFeign.groovy
+++ payroll-api/src/main/groovy/br/com/humanize/payrollapi/feignClients/UserFeign.groovy
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "user-api", url = "http://localhost:8000")
+@FeignClient(name = "user-api")
 interface UserFeign {
 
     @GetMapping(value = "/api/users/{id}")
