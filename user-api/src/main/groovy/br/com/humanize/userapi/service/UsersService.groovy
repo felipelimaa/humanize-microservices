3c1a3163919ea09426330acabcd27e41bbd23e3f
felipe <fearlimasi@gmail.com>
2022-03-02 22:53:22 -0300

    feat: configurated eureka client in user-api

--- user-api/src/main/groovy/br/com/humanize/userapi/service/UsersService.groovy
+++ user-api/src/main/groovy/br/com/humanize/userapi/service/UsersService.groovy
@@ -3,16 +3,23 @@ package br.com.humanize.userapi.service
 import br.com.humanize.userapi.domain.Users
 import br.com.humanize.userapi.repository.UsersRepository
 import br.com.humanize.userapi.service.exception.EntityNotFoundException
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 
+@Slf4j
 @Service
 class UsersService {
 
     @Autowired
     UsersRepository userRepository
 
+    @Autowired
+    Environment env
+
     Users findById(Long id) {
+        log.info("USER_SERVICE ::: Get Request on " + env.getProperty("local.server.port") + " port.")
         return userRepository.findById(id).orElseThrow{new EntityNotFoundException()}
     }
 
